b42c272522d6c05f1f0656d623997d5b8406ca0a
abuziming <255670851@qq.com>
2022-08-22 19:52:09 +0800

    配置

---
+++ src/main/java/com/myblog/blog/config/WebMvcConfig.java
@@ -0,0 +1,15 @@
+package com.myblog.blog.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+   //跨域配置
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        WebMvcConfigurer.super.addCorsMappings(registry);
+        registry.addMapping("/**").allowedOrigins("http://localhost:8080");
+    }
+}
